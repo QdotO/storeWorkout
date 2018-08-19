cc6bec77cbd71168798f1f44aee0517011a09227
Quincy <quincyobeng@gmail.com>
2018-08-19 18:55:46 -0500

    Updated the updateLambda script

--- updateLambda.sh
+++ updateLambda.sh
@@ -1,2 +1,12 @@
-zip -r storeworkout.zip .
+#!/bin/bash
+file=./storeworkouts.zip
+if [ -e "$file" ]; then
+    echo "zip already exists"
+    echo "Erasing...."
+    rm -r storeworkouts.zip
+fi 
+
+zip -r storeworkout.zip . -x *.git*
+echo "All zipped up"
+echo "Now deploying..."
 aws lambda update-function-code --function-name StoreWorkout --zip-file fileb://storeworkout.zip
